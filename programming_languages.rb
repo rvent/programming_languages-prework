7d820273c1f2a08c783cfec3c7c054c8af94728a
Rene Ventura <venturar729@gmail.com>
2018-12-25 20:33:22 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -33,13 +33,19 @@ languages = {
 def reformat_languages(languages)
   # your code here
   new_hash = {}
-  language_type = languages.keys
+  language_style = languages.keys
   language_values = languages.values
   language_name = language_values.map {|value| value.keys}
-  language_name.flatten.each do |name|
+  language_name.flatten.uniq.each do |name|
+    new_hash[name][:style] = []
     language_values.each do |value|
       if value[name] != nil
         new_hash[name] = value[name]
+      end
+    end
+    language_style.each do |key|
+      if languages[key].include?(name)
+        new_hash[name][:style] << key
       end
     end
   end
