d10e7c112801c126ba36c150059ab92c8d63175a
Rene Ventura <venturar729@gmail.com>
2018-12-25 20:41:33 +0000

    Automatically backed up by Learn

---
+++ programming_languages_second_version.rb
@@ -0,0 +1,21 @@
+def reformat_languages(languages)
+  # your code here
+  new_hash = {}
+  language_style = languages.keys
+  language_values = languages.values
+  language_name = language_values.map {|value| value.keys}
+  language_name.flatten.uniq.each do |name|
+    language_values.each do |value|
+      if value[name] != nil
+        new_hash[name] = value[name]
+      end
+    end
+    new_hash[name][:style] = []
+    language_style.each do |key|
+      if languages[key].include?(name)
+        new_hash[name][:style] << key
+      end
+    end
+  end
+  new_hash
+end
